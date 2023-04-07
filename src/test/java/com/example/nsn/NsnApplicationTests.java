04c63979eb34d04ecda213d6fe592026314db027
Loy721 <g.a.1912@mail.ru>
2023-04-07 21:20:17 +0300

    Initial commit

---
+++ src/test/java/com/example/nsn/NsnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
